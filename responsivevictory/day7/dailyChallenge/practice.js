0c7dc7e460d7a4b51ebf6f2bea6d727a563a8fe5
kersea <kerseaw@gmail.com]>
2023-04-27 19:37:38 -0700

    javascriptpractice1

---
+++ responsivevictory/day7/dailyChallenge/practice.js
@@ -0,0 +1,11 @@
+var firstName = "Kersea";
+var lastName = "Washington";
+var Fullname = `${firstName} ${lastName}`;
+
+    console.log(Fullname);
+
+var luckyNumberOne = 1
+var luckyNumberTwo = 7
+
+console.log( `My first lucky number is ${luckyNumberOne} my second favorite lucky number is ${luckyNumberTwo}`)
+ 
